e4d82712c8aeb7be5b4c53404c1da1e051a0fda3
Harrison Chase <hw.chase.17@gmail.com>
2019-11-14 23:17:37 -0500

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup, find_packages
 
+with open('README.rst', 'r') as f:
+    long_description = f.read()
+
 setup(name='pysportsref',
       version_format='0.0.1',
       packages=find_packages(),
       description='Utilities for working with Sports Reference',
-      install_requires=['bs4', 'numpy', 'pandas', 'requests', 'lxml', 'tqdm'])
+      install_requires=['bs4', 'numpy', 'pandas', 'requests', 'lxml', 'tqdm'],
+      long_description=long_description,
+      license='MIT',
+      url='https://github.com/hwchase17/pysportsref')
